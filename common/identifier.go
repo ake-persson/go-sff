e918ffe3f717e43d4d0f8d54efa2ac48fa47f4e3
Michael Persson <michael.ake.persson@gmail.com>
2017-10-23 13:11:43 -0500

    Correct camel case on const

--- common/identifier.go
+++ common/identifier.go
@@ -7,56 +7,56 @@ import (
 
 const (
 	IdentifierUnknown    = 0x00
-	IdentifierGBIC       = 0x01
+	IdentifierGbic       = 0x01
 	IdentifierSoldered   = 0x02
-	IdentifierSFP        = 0x03
-	Identifier300PinXBI  = 0x04
-	IdentifierXENPAK     = 0x05
-	IdentifierXFP        = 0x06
-	IdentifierXFF        = 0x07
-	IdentifierXFPE       = 0x08
-	IdentifierXPAK       = 0x09
+	IdentifierSfp        = 0x03
+	Identifier300PinXbi  = 0x04
+	IdentifierXenpak     = 0x05
+	IdentifierXfp        = 0x06
+	IdentifierXff        = 0x07
+	IdentifierXfpE       = 0x08
+	IdentifierXpak       = 0x09
 	IdentifierX2         = 0x0A
-	IdentifierDWDMSFP    = 0x0B
-	IdentifierQSFP       = 0x0C
-	IdentifierQSFPPlus   = 0x0D
-	IdentifierCXP        = 0x0E
-	IdentifierHD4X       = 0x0F
-	IdentifierHD8X       = 0x10
-	IdentifierQSFP28     = 0x11
-	IdentifierCXP2       = 0x12
-	IdentifierCDFP       = 0x13
-	IdentifierHD4XFanout = 0x14
-	IdentifierHD8XFanout = 0x15
-	IdentifierCDFPStyle3 = 0x16
-	IdentifierMicroQSFP  = 0x17
+	IdentifierDwdmSfp    = 0x0B
+	IdentifierQsfp       = 0x0C
+	IdentifierQsfpPlus   = 0x0D
+	IdentifierCxp        = 0x0E
+	IdentifierHd4x       = 0x0F
+	IdentifierHd8x       = 0x10
+	IdentifierQsfp28     = 0x11
+	IdentifierCxp2       = 0x12
+	IdentifierCdfp       = 0x13
+	IdentifierHd4xFanout = 0x14
+	IdentifierHd8xFanout = 0x15
+	IdentifierCdfpStyle3 = 0x16
+	IdentifierMicroQsfp  = 0x17
 )
 
 var identifierNames = map[byte]string{
 	IdentifierUnknown:    "No module present, unknown, or unspecified",
-	IdentifierGBIC:       "GBIC",
+	IdentifierGbic:       "GBIC",
 	IdentifierSoldered:   "Module soldered to motherboard",
-	IdentifierSFP:        "SFP",
-	Identifier300PinXBI:  "300 pin XBI",
-	IdentifierXENPAK:     "XENPAK",
-	IdentifierXFP:        "XFP",
-	IdentifierXFF:        "XFF",
-	IdentifierXFPE:       "XFP-E",
-	IdentifierXPAK:       "XPAK",
+	IdentifierSfp:        "SFP",
+	Identifier300PinXbi:  "300 pin XBI",
+	IdentifierXenpak:     "XENPAK",
+	IdentifierXfp:        "XFP",
+	IdentifierXff:        "XFF",
+	IdentifierXfpE:       "XFP-E",
+	IdentifierXpak:       "XPAK",
 	IdentifierX2:         "X2",
-	IdentifierDWDMSFP:    "DWDM-SFP",
-	IdentifierQSFP:       "QSFP",
-	IdentifierQSFPPlus:   "QSFP+",
-	IdentifierCXP:        "CXP",
-	IdentifierHD4X:       "Shielded Mini Multilane HD 4X",
-	IdentifierHD8X:       "Shielded Mini Multilane HD 8X",
-	IdentifierQSFP28:     "QSFP28",
-	IdentifierCXP2:       "CXP2/CXP28",
-	IdentifierCDFP:       "CDFP Style 1/Style 2",
-	IdentifierHD4XFanout: "Shielded Mini Multilane HD 4X Fanout Cable",
-	IdentifierHD8XFanout: "Shielded Mini Multilane HD 8X Fanout Cable",
-	IdentifierCDFPStyle3: "CDFP Style 3",
-	IdentifierMicroQSFP:  "MicroQSFP",
+	IdentifierDwdmSfp:    "DWDM-SFP",
+	IdentifierQsfp:       "QSFP",
+	IdentifierQsfpPlus:   "QSFP+",
+	IdentifierCxp:        "CXP",
+	IdentifierHd4x:       "Shielded Mini Multilane HD 4X",
+	IdentifierHd8x:       "Shielded Mini Multilane HD 8X",
+	IdentifierQsfp28:     "QSFP28",
+	IdentifierCxp2:       "CXP2/CXP28",
+	IdentifierCdfp:       "CDFP Style 1/Style 2",
+	IdentifierHd4xFanout: "Shielded Mini Multilane HD 4X Fanout Cable",
+	IdentifierHd8xFanout: "Shielded Mini Multilane HD 8X Fanout Cable",
+	IdentifierCdfpStyle3: "CDFP Style 3",
+	IdentifierMicroQsfp:  "MicroQSFP",
 }
 
 type Identifier byte
--- sff8636/link_codes.go
+++ sff8636/link_codes.go
@@ -7,56 +7,56 @@ import (
 
 const (
 	LinkCodesUnspecified    = 0x00
-	LinkCodes100GAOC        = 0x01
-	LinkCodes100GSR4        = 0x02
-	LinkCodes100GLR4        = 0x03
-	LinkCodes100GER4        = 0x04
-	LinkCodes100GSR10       = 0x05
-	LinkCodes100GCWDM4FEC   = 0x06
-	LinkCodes100GPSM4       = 0x07
-	LinkCodes100GACC        = 0x08
-	LinkCodes100GCWDM4NoFEC = 0x09
-	LinkCodes100GRSVD1      = 0x0A
-	LinkCodes100GCR4        = 0x0B
-	LinkCodes25GCRCAS       = 0x0C
-	LinkCodes25GCRCAN       = 0x0D
-	LinkCodes40GER4         = 0x10
-	LinkCodes4X10SR         = 0x11
-	LinkCodes40GPSM4        = 0x12
-	LinkCodesG959P1I12D1    = 0x13
-	LinkCodesG959P1S12D2    = 0x14
-	LinkCodesG959P1L12D2    = 0x15
-	LinkCodes10GTSFI        = 0x16
-	LinkCodes100GCLR4       = 0x17
-	LinkCodes100GAOC2       = 0x18
-	LinkCodes100GACC2       = 0x19
+	LinkCodes100gAoc        = 0x01
+	LinkCodes100gSr4        = 0x02
+	LinkCodes100gLr4        = 0x03
+	LinkCodes100gEr4        = 0x04
+	LinkCodes100gSr10       = 0x05
+	LinkCodes100gCwdm4Fec   = 0x06
+	LinkCodes100gPsm4       = 0x07
+	LinkCodes100gAcc        = 0x08
+	LinkCodes100gCwdm4NoFec = 0x09
+	LinkCodes100gRsvd1      = 0x0A
+	LinkCodes100gCr4        = 0x0B
+	LinkCodes25gCrCaS       = 0x0C
+	LinkCodes25gCrCaN       = 0x0D
+	LinkCodes40gEr4         = 0x10
+	LinkCodes4X10Sr         = 0x11
+	LinkCodes40gPsm4        = 0x12
+	LinkCodesG959P1t12d1    = 0x13
+	LinkCodesG959P1s12d2    = 0x14
+	LinkCodesG959P1l12d2    = 0x15
+	LinkCodes10gTSfi        = 0x16
+	LinkCodes100gClr4       = 0x17
+	LinkCodes100gAoc2       = 0x18
+	LinkCodes100gAcc2       = 0x19
 )
 
 var linkCodesNames = map[byte]string{
 	LinkCodesUnspecified:    "Reserved or unknown",
-	LinkCodes100GAOC:        "100G LinkCodes: 100G AOC or 25GAUI C2M AOC with worst BER of 5x10^(-5)",
-	LinkCodes100GSR4:        "100G LinkCodes: 100G Base-SR4 or 25GBase-SR",
-	LinkCodes100GLR4:        "100G LinkCodes: 100G Base-LR4",
-	LinkCodes100GER4:        "100G LinkCodes: 100G Base-ER4",
-	LinkCodes100GSR10:       "100G LinkCodes: 100G Base-SR10",
-	LinkCodes100GCWDM4FEC:   "100G LinkCodes: 100G CWDM4 MSA with FEC",
-	LinkCodes100GPSM4:       "100G LinkCodes: 100G PSM4 Parallel SMF",
-	LinkCodes100GACC:        "100G LinkCodes: 100G ACC or 25GAUI C2M ACC with worst BER of 5x10^(-5)",
-	LinkCodes100GCWDM4NoFEC: "100G LinkCodes: 100G CWDM4 MSA without FEC",
-	LinkCodes100GRSVD1:      "(reserved or unknown)",
-	LinkCodes100GCR4:        "100G LinkCodes: 100G Base-CR4 or 25G Base-CR CA-L",
-	LinkCodes25GCRCAS:       "25G LinkCodes: 25G Base-CR CA-S",
-	LinkCodes25GCRCAN:       "25G LinkCodes: 25G Base-CR CA-N",
-	LinkCodes40GER4:         "40G LinkCodes: 40G Base-ER4",
-	LinkCodes4X10SR:         "4x10G LinkCodes: 10G Base-SR",
-	LinkCodes40GPSM4:        "40G LinkCodes: 40G PSM4 Parallel SMF",
-	LinkCodesG959P1I12D1:    "LinkCodes: G959.1 profile P1I1-2D1 (10709 MBd, 2km, 1310nm SM)",
-	LinkCodesG959P1S12D2:    "LinkCodes: G959.1 profile P1S1-2D2 (10709 MBd, 40km, 1550nm SM)",
-	LinkCodesG959P1L12D2:    "LinkCodes: G959.1 profile P1L1-2D2 (10709 MBd, 80km, 1550nm SM)",
-	LinkCodes10GTSFI:        "10G LinkCodes: 10G Base-T with SFI electrical interface",
-	LinkCodes100GCLR4:       "100G LinkCodes: 100G CLR4",
-	LinkCodes100GAOC2:       "100G LinkCodes: 100G AOC or 25GAUI C2M AOC with worst BER of 10^(-12)",
-	LinkCodes100GACC2:       "100G LinkCodes: 100G ACC or 25GAUI C2M ACC with worst BER of 10^(-12)",
+	LinkCodes100gAoc:        "100G LinkCodes: 100G AOC or 25GAUI C2M AOC with worst BER of 5x10^(-5)",
+	LinkCodes100gSr4:        "100G LinkCodes: 100G Base-SR4 or 25GBase-SR",
+	LinkCodes100gLr4:        "100G LinkCodes: 100G Base-LR4",
+	LinkCodes100gEr4:        "100G LinkCodes: 100G Base-ER4",
+	LinkCodes100gSr10:       "100G LinkCodes: 100G Base-SR10",
+	LinkCodes100gCwdm4Fec:   "100G LinkCodes: 100G CWDM4 MSA with FEC",
+	LinkCodes100gPsm4:       "100G LinkCodes: 100G PSM4 Parallel SMF",
+	LinkCodes100gAcc:        "100G LinkCodes: 100G ACC or 25GAUI C2M ACC with worst BER of 5x10^(-5)",
+	LinkCodes100gCwdm4NoFec: "100G LinkCodes: 100G CWDM4 MSA without FEC",
+	LinkCodes100gRsvd1:      "(reserved or unknown)",
+	LinkCodes100gCr4:        "100G LinkCodes: 100G Base-CR4 or 25G Base-CR CA-L",
+	LinkCodes25gCrCaS:       "25G LinkCodes: 25G Base-CR CA-S",
+	LinkCodes25gCrCaN:       "25G LinkCodes: 25G Base-CR CA-N",
+	LinkCodes40gEr4:         "40G LinkCodes: 40G Base-ER4",
+	LinkCodes4X10Sr:         "4x10G LinkCodes: 10G Base-SR",
+	LinkCodes40gPsm4:        "40G LinkCodes: 40G PSM4 Parallel SMF",
+	LinkCodesG959P1t12d1:    "LinkCodes: G959.1 profile P1I1-2D1 (10709 MBd, 2km, 1310nm SM)",
+	LinkCodesG959P1s12d2:    "LinkCodes: G959.1 profile P1S1-2D2 (10709 MBd, 40km, 1550nm SM)",
+	LinkCodesG959P1l12d2:    "LinkCodes: G959.1 profile P1L1-2D2 (10709 MBd, 80km, 1550nm SM)",
+	LinkCodes10gTSfi:        "10G LinkCodes: 10G Base-T with SFI electrical interface",
+	LinkCodes100gClr4:       "100G LinkCodes: 100G CLR4",
+	LinkCodes100gAoc2:       "100G LinkCodes: 100G AOC or 25GAUI C2M AOC with worst BER of 10^(-12)",
+	LinkCodes100gAcc2:       "100G LinkCodes: 100G ACC or 25GAUI C2M ACC with worst BER of 10^(-12)",
 }
 
 type LinkCodes byte
